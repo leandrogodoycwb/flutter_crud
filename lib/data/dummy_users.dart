0f358e320c707040f1c31f21709ba9bc16b0d93e
LEANDRO\Leandro L. Godoy <leandrolenon@gmail.com>
2021-11-02 01:35:38 -0300

    segundo commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@alunos.com.br',
+    avatarUrl: 'https://a.espncdn.com/i/teamlogos/soccer/500/3456.png'
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Rafael',
+    email: 'rafael@alunos.com.br',
+    avatarUrl: 'https://static.cbncuritiba.com/cbn/wp-content/uploads/2020/09/Paran%C3%A1-Clube-1.png'
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Fernanda',
+    email: 'fernanda@zymail.com.br',
+    avatarUrl: 'https://www.athletico.com.br/wp-content/themes/hurricane/assets/images/logo-cap-white.png'
+  ),
+  '4': const User(
+    id: '4',
+    name: 'Operario',
+    email: 'jose@zymail.com.br',
+    avatarUrl: 'https://upload.wikimedia.org/wikipedia/pt/1/16/Oper%C3%A1rioFerrovi%C3%A1rioEC%282018%29.png'
+  ),
+};
